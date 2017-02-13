c6847ae4b92bd063f20f1ba1def5fcc660db0f01
Billy Rohrkaste <williamrohrkaste@gmail.com>
2017-02-13 17:58:44 -0600

    Add files via upload

---
+++ degreeCheck.js
@@ -0,0 +1,31 @@
+//Core CS classes
+var cs111 = {
+                grade : null,
+                title : "Intro to Computing I",
+                dept : "CS"
+            }
+
+var cs140 = {
+                grade : null,
+                title : "Intro to Computing II",
+                dept : "CS"
+            }
+
+var cs150 = {   
+                grade : null,
+                title : "Intro to Computing I",
+                dept : "CS"
+            }
+
+function submitClasses() {
+
+    cs111.grade = document.getElementById('cs111').value;
+    document.getElementById("error").innerHTML = cs111.grade;
+}
+
+function evaluateDegree() {
+
+
+
+}
+
